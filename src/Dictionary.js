c7fd95d3356aa33ccbca17c262909f2d71054cd6
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-02-02 15:12:59 -0600

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,16 @@
 import react from "react";
 
 export default function Dictionary(){
+   function search(event){
+       event.preventDefault();
+       alert("Searching");
+   }
+   
+   
     return (
         <div className="Dictionary">
             <form>
-                <input type="search"/>
+                <input type="search" autoFocus={true}/>
             </form>
         </div>
     );
