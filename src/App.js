f4e32e83c9a0747e0a6dd60ac63af271b7d7ca51
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-02-02 15:04:59 -0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
-<h1>hello</h1>
+
       </header>
+      <main>
+        <Dictionary/>
+        </main>
+        <footer className="text-center"> Coded by Izeta Softic
+        
+        </footer>
+      </div>
     </div>
   );
 }
