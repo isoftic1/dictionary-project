7b503fa847624006942ca1485bf3cd2624cd46b5
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-02-07 11:39:00 -0600

    Rendered the word meanings

---
+++ src/Meaning.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+export default function Meaning (props){
+    return (
+        <div className="Meanings">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        {props.meaning.definitions.map(function(definition, index){
+            return (
+                <div key={index}>
+                    <p>
+                    {definition.definition}
+                    <br/>
+                    <em>{definition.example}</em>
+                    </p>
+                    </div>
+            );
+        })}
+        </div>
+    );
+}
