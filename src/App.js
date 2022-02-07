e9c6207795d49b29392fbba9be69439afe9f5392
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-02-07 12:20:55 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
       </header>
       <main>
-        <Dictionary />
+        <Dictionary/>
         </main>
         <footer className="text-center"> Coded by Izeta Softic
         
