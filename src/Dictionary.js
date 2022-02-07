66d0ff4dfe745257654601f69b4729b38db09f46
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-02-07 11:36:31 -0600

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,15 +4,15 @@ import axios from "axios";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
-    let [results, setResults] = useState({});
+    let [results, setResults] = useState(null);
+
    function handleResponse(response){
-       console.log(response.data[0]);
        setResults(response.data[0]);
    
    }
     function search(event){
-       event.preventDefault();
-       alert(`Searching for ${keyword}`);
+        event.preventDefault();
+        
        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
        axios.get(apiUrl).then(handleResponse);
    }
@@ -25,7 +25,7 @@ export default function Dictionary(){
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange} />
             </form>
-            <Results /> 
+            <Results results={results}/>
         </div>
     );
 }
