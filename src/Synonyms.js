5264d1f03030dd45af75b89743de98ab260ff80d
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-03-29 14:33:34 -0500

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,7 @@ import react from "react";
 
 export default function Synonyms(props){
     if (props.synonyms){
-        return "Hello from Synonyms";
+        return props.synonyms;
     } else{
         return null;
     }
