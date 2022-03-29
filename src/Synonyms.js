2ad16e21466e79662f55ec25779a81e78642fbfe
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-03-29 14:45:59 -0500

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,17 @@ import React from "react";
 
 export default function Synonyms(props){
     if (props.synonyms){
-        return props.synonyms;
+        return (
+            <div className="Synonyms">
+ {props.synonyms.map(function(synonym, index){
+     return(
+         <span key={index}>
+         {synonym}
+         </span>
+     )
+ })}
+ </div>
+        );
     } else{
         return null;
     }
