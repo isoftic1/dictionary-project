43e7d65542d4d73d778f0cb893e2ef27fb430ab4
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-03-29 16:18:38 -0500

    Create Phonetic.js

---
+++ src/Phonetic.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Phonetic(props) {
+
+    if (props.phonetic.audio) {
+        return (
+            <div className="phonetic">
+                <audio className="phonetic container" controls src={props.phonetic.audio} />
+            </div>
+                );
+    } else {
+        return null;
+    }
+}
