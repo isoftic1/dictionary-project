df838c4a42dc90163b0031bfb62fefeaf334ffe5
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-03-29 16:00:28 -0500

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,8 +5,9 @@ export default function Synonyms(props) {
         return (
             <ul className="Synonyms">
             {props.synonyms.map(function (synonym, index) {
-                return <li key={index}>{synonym}</li>;
-                
+                return(
+                <li key={index}>{synonym}</li>
+                )    
             })}
             </ul>
         );
