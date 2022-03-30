f8bb6bb4da906b89ea858b15dd7d365704d77d53
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-03-30 14:05:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import Dictionary from "./Dictionary";
 import './App.css';
+import logo from "./logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <div className='container'>
-      <header className="App-header">
-
-      </header>
-      <main>
-        <Dictionary/>
-        </main>
-        <footer className="text-center"> Coded by Izeta Softic
-        
+    <div className="container">
+    <header>
+      <div className="App mt-5">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </div>
+    </header>
+    <main>
+      <Dictionary  />
+    </main>
+        <footer className="text-center">
+        This website was designed and coded by UX Web Designer
+         <a href="https://www.linkedin.com/in/izeta-softic-276531220/" target="_blank"
+          rel="noreferrer"> Izeta Softic</a>, and is 
+          <a href="https://github.com/isoftic1/dictionary-project.git" 
+          target="_blank" rel="noreferrer"> open-sourced</a>.
         </footer>
       </div>
-    </div>
+    
   );
 }
 
