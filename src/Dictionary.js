352cd0fd970ad1d5df63010637f883f8c2b70908
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-03-30 14:05:32 -0500

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Results from "./Results";
 import Photos from "./Photos";
+import "./Dictionary.css";
 import axios from "axios";
 
 export default function Dictionary(){
@@ -23,7 +24,7 @@ export default function Dictionary(){
        axios.get(apiUrl).then(handleDictionaryResponse);
      
        let pexelsApiKey ="563492ad6f917000010000017167b79be6c241cdaeb67ce2eb0d75d1";
-       let pexelsApiUrl= `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+       let pexelsApiUrl= `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
        let headers = {Authorization: `Bearer ${pexelsApiKey}`};
        axios.get(pexelsApiUrl, {headers:headers}).then(handlePexelsResponse);
     }
@@ -33,11 +34,23 @@ export default function Dictionary(){
    
     return (
         <div className="Dictionary">
-            <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange} />
+          <div className="container-box">
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={search}>
+              <div className="d-flex">
+                  <div className="flex-grow-1">
+                  <input type="search" placeholder="Search" className="form-control" onChange={handleKeywordChange} />
+                  </div>
+                  <div className="mx-1">
+                  <button value="Search" className="btn mx-1 px-4  text-center" type="submit">Search</button>
+                  </div>
+              </div>
             </form>
+            <div className="suggestions mt-2">Such as: Sunset, nature, ocean </div>
+            </div>
             <Results results={results}/>
             <Photos photos={photos}/>
-        </div>
+          </div>
+        
     );
-}
+} 
