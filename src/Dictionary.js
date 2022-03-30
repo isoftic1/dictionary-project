f82101f1a7c562a557513ea497c60b0f4a60f438
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-03-29 21:28:36 -0500

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,32 @@
 import React, {useState} from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
 
-   function handleResponse(response){
+   function handleDictionaryResponse(response){
        setResults(response.data[0]);
    
+   }
+   function handlePexelsResponse(response){
+       setPhotos(response.data.photos);
+
    }
     function search(event){
         event.preventDefault();
         
        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-       axios.get(apiUrl).then(handleResponse);
-   }
+       axios.get(apiUrl).then(handleDictionaryResponse);
+     
+       let pexelsApiKey ="563492ad6f917000010000017167b79be6c241cdaeb67ce2eb0d75d1";
+       let pexelsApiUrl= `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+       let headers = {Authorization: `Bearer ${pexelsApiKey}`};
+       axios.get(pexelsApiUrl, {headers:headers}).then(handlePexelsResponse);
+    }
    function handleKeywordChange(event){
        setKeyword(event.target.value);
    }
@@ -26,6 +37,7 @@ export default function Dictionary(){
                 <input type="search" onChange={handleKeywordChange} />
             </form>
             <Results results={results}/>
+            <Photos photos={photos}/>
         </div>
     );
 }
